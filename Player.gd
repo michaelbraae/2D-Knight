41ef01099805f6934c1c46c3c52b548b481618d6
michael <michaelbraae@gmail.com>
2020-05-11 16:26:48 +1200

    added the wooden chest object, the skele and archer npcs

--- Player.gd
+++ Player.gd
@@ -5,12 +5,15 @@ const GRAVITY = 10
 const SPEED = 180
 const JUMP_HEIGHT = -200
 onready var animatedSprite = $AnimatedSprite
+onready var attackBox = $AttackBox
 var doubleJumped = false
+onready var attackBoxOriginX = attackBox.position.x
+
 # onready var hitBox = $HitBox
 var rolling = false
 var attacking = false
 var nextAttack = "attack1" # so we always start from the first attack
-
+var direction = 1
 var motion = Vector2()
 
 func _process(_delta):
@@ -21,20 +24,26 @@ func _process(_delta):
 
 func getMotion():
 	rolling = false
+	motion.x = SPEED * direction
 	if Input.is_action_pressed("left_click"):
 		attack()
 	elif Input.is_action_pressed("ui_right"):
+		
+		direction = 1
+		animatedSprite.position.x = 0
+		attackBox.position.x = 42
 		animatedSprite.play("run")
 		animatedSprite.flip_h = false
-		motion.x = SPEED
 	elif Input.is_action_pressed("left"):
+		direction = -1
+		animatedSprite.position.x = 18
+		attackBox.position.x = -23
 		animatedSprite.play("run")
 		animatedSprite.flip_h = true
-		motion.x = -SPEED
 	else:
 		animatedSprite.play("idle")
 		motion.x = 0
-
+	
 func _physics_process(_delta):
 	motion.y += GRAVITY
 	if animatedSprite.get_animation() == "roll":
@@ -42,8 +51,7 @@ func _physics_process(_delta):
 	elif attacking == true:
 		animatedSprite.play("attack1")
 	else:
-		getMotion()
-				
+		getMotion()	
 		if Input.is_action_pressed("roll"):
 				animatedSprite.play("roll")
 		
@@ -65,7 +73,6 @@ func _physics_process(_delta):
 			animatedSprite.play("jump")
 		
 	motion = move_and_slide(motion, UP)
-	pass
 
 func attack():
 	var overlappingAreas = PlayerAttackBox.get_overlapping_areas()
@@ -84,3 +91,6 @@ func _on_AnimatedSprite_animation_finished():
 	animatedSprite.play("idle")
 	rolling = false
 	attacking = false
+
+func isPlayer():
+	pass
--- res/NPC/Dark Knight/DarkKnight.gd
+++ res/NPC/Dark Knight/DarkKnight.gd
@@ -1,16 +1,25 @@
 extends KinematicBody2D
 
-var health = 10
-const GRAVITY = 1
-export (int) var speed = 200
+const GRAVITY = 10
 const UP = Vector2(0, -1)
-var velocity = Vector2()
+const SPEED = 100
+
+var motion = Vector2()
+
+var health = 10
 var player_detected = false
+var hit = false
+var direction = 1
+var sightCastToY = 250
+
 onready var animatedSprite = $AnimatedSprite
 onready var healthSprite = $HealthSprite
-var hit = false
+onready var ledgeRayCast = $LedgeRayCast
+onready var sightRayCast = $SightRayCast
+onready var Player = get_node("Player")
 
 func takeHit(damage):
+	player_detected = true
 	hit = true
 	health = health - damage
 	if health < 0:
@@ -20,17 +29,32 @@ func takeHit(damage):
 	else:
 		animatedSprite.play("death")
 
+func bounceOffWallsAndLedges():
+	if is_on_wall():
+		direction = direction * -1
+		ledgeRayCast.position.x *= -1
+		sightCastToY = sightCastToY * -1
+		
+	if ledgeRayCast.is_colliding() == false:
+		sightCastToY = sightCastToY * -1
+		direction = direction * -1
+		ledgeRayCast.position.x *= -1
+	sightRayCast.set_cast_to(Vector2(0, sightCastToY))
+
 func _process(_delta):
 	healthSprite.play(str(health))
 
+func attack():
+	pass
+
 func _physics_process(_delta):
-	velocity = Vector2()
-	velocity.y += GRAVITY
+	motion.y += GRAVITY
+	motion.x = SPEED * direction
 	if not hit:
-		if velocity.x > 0:
+		if motion.x > 0:
 			animatedSprite.flip_h = false
 			animatedSprite.play("run")
-		elif velocity.x < 0:
+		elif motion.x < 0:
 			animatedSprite.flip_h = true
 			animatedSprite.play("run")
 		else:
@@ -39,8 +63,15 @@ func _physics_process(_delta):
 	if not is_on_floor():
 		animatedSprite.play("fall")
 	
-	velocity = velocity.normalized() * speed
-	velocity = move_and_slide(velocity, UP)
+	motion = motion.normalized() * SPEED
+	motion = move_and_slide(motion, UP)
+	
+	bounceOffWallsAndLedges()
+	
+	if sightRayCast.is_colliding():
+		var collidingObject = sightRayCast.get_collider()
+		if collidingObject.has_method("isPlayer"):
+			player_detected = true
 
 func _on_AnimatedSprite_animation_finished():
 	var animation = animatedSprite.get_animation()
